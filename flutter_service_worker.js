eae9db3c5f333a6acd635e0dbed2acdd41edb694
quan0715 <quan0715@users.noreply.github.com>
2023-10-21 07:34:09 +0000

    deploy: 94ac4a05c1af0198812bd4364fdddc5ddfedb410

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,8 +3,8 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"index.html": "7c13778a3621f2bff7831a121d4ce3fc",
-"/": "7c13778a3621f2bff7831a121d4ce3fc",
+const RESOURCES = {"index.html": "e9aa3a27b8e3dbd40b8342117df7e756",
+"/": "e9aa3a27b8e3dbd40b8342117df7e756",
 "version.json": "aaedac1d64807806012e3e33f63512cb",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "assets/AssetManifest.bin": "e4d57616ca8f53898ed2b85c29d86802",
