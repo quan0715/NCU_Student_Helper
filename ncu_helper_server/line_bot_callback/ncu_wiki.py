955d65253155a3ab44865579f657388e9d98792b
an920107 <an920107@gmail.com>
2023-10-22 02:00:47 +0800

    quan wiki and requirements

--- ncu_helper_server/line_bot_callback/ncu_wiki.py
+++ ncu_helper_server/line_bot_callback/ncu_wiki.py
@@ -1,6 +1,7 @@
+from pydantic import BaseModel, Field
 from langchain.embeddings.openai import OpenAIEmbeddings
-from langchain.document_loaders import TextLoader, PyPDFLoader, CSVLoader
-from langchain.text_splitter import  RecursiveCharacterTextSplitter
+from langchain.document_loaders import TextLoader, PyPDFLoader, CSVLoader, JSONLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.vectorstores import Chroma
 from langchain.chat_models import ChatOpenAI
 from langchain.chains import ConversationalRetrievalChain
@@ -13,20 +14,21 @@ import chromadb
 
 persistent_client = chromadb.PersistentClient()
 
-from pydantic import BaseModel, Field
 # load_dotenv()
 api_key = os.getenv('OPEN_AI_API_KEY')
 
 PDF_PATH_NAME = "docs/food.pdf"
 
+
 class Document(BaseModel):
-  """Interface for interacting with a document."""
-  page_content: str
-  metadata: dict = Field(default_factory=dict)
-  
+    """Interface for interacting with a document."""
+    page_content: str
+    metadata: dict = Field(default_factory=dict)
+
+
 def loadDocument(path: str):
-    loader = PyPDFLoader(path) 
-    splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=0) 
+    loader = PyPDFLoader(path)
+    splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=0)
     document = loader.load_and_split(splitter)
     vectordb = Chroma.from_documents(
         document,
@@ -35,6 +37,7 @@ def loadDocument(path: str):
     )
     vectordb.persist()
     # return vectordb
+
 
 def load_all_documents():
     documents = []
@@ -55,7 +58,12 @@ def load_all_documents():
             text_path = 'line_bot_callback/docs/' + file
             loader = CSVLoader(text_path)
             documents.extend(loader.load())
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=10)
+        elif file.endswith('.json'):
+            json_path = 'line_bot_callback/docs/' + file
+            loader = JSONLoader(json_path)
+            documents.extend(loader)
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=1000, chunk_overlap=10)
     chunked_documents = text_splitter.split_documents(documents)
     vectordb = Chroma.from_documents(
         chunked_documents,
@@ -65,17 +73,19 @@ def load_all_documents():
     vectordb.persist()
     return vectordb
 
+
 def getWikiChainLLM():
     # db = loadDocument("docs/network_issue.pdf")
     # client = chromadb.PersistentClient(path="/path/to/save/to")
     db = load_all_documents()
-    db = Chroma(persist_directory="line_bot_callback/data", embedding_function=OpenAIEmbeddings(openai_api_key=api_key))
+    db = Chroma(persist_directory="line_bot_callback/data",
+                embedding_function=OpenAIEmbeddings(openai_api_key=api_key))
     return RetrievalQA.from_chain_type(
-        llm = ChatOpenAI(openai_api_key=api_key),
-        retriever= db.as_retriever(search_kwargs = {'k': 3}),
+        llm=ChatOpenAI(openai_api_key=api_key),
+        retriever=db.as_retriever(search_kwargs={'k': 3}),
         return_source_documents=True,
     )
-    
+
     # result = chain({'query': '條列幾間中壢好吃的美食'})
     # print(result['result'])
     # result = chain({'query': '要如何知道自己的mac address呢'})
@@ -83,17 +93,16 @@ def getWikiChainLLM():
     # result = chain({
     #     'query': '給我火舞社相關資訊'
     # })
-    
+
     # print(result['result'])
     # response = chain.run(input_documents=documents, question=query)
-    
+
 # if __name__ == "__main__":
 #     runLLM()
-    
-    
-    #embeddings = OpenAIEmbeddings(openai_api_key=api_key)
-    
+
+    # embeddings = OpenAIEmbeddings(openai_api_key=api_key)
+
     # qa = ConversationalRetrievalChain.from_llm(ChatOpenAI(temperature=0, openai_api_key=api_key), vectorstore.as_retriever())
 
-    #result = qa({"question": query, 'chat_history': []})
-    #print('A:', result['answer'])
+    # result = qa({"question": query, 'chat_history': []})
+    # print('A:', result['answer'])
